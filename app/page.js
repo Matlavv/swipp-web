0104904d6c90e2d7ce5f34c05e277959b59dba14
matlavv <mathis.laversin92@gmail.com>
2024-03-14 12:37:25 +0100

    Three container in homepage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 "use client";
 import Header from "@/components/Header";
 import { PhoneMockup } from "@/components/PhoneMockup";
+import { ThreeContainer } from "@/components/ThreeContainer";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <PhoneMockup />
+      <ThreeContainer />
     </div>
   );
 }
