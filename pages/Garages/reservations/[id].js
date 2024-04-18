6d8dd98650ca17a18fafa63bffb4151708c95757
matlavv <mathis.laversin92@gmail.com>
2024-04-18 12:04:51 +0200

    Get vehicles informations

--- pages/Garages/reservations/[id].js
+++ pages/Garages/reservations/[id].js
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useAuth } from "@/utils/AuthContext";
 import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -8,8 +9,10 @@ import { db } from "../../../utils/firebaseConfig";
 const DetailedReservation = () => {
   const [reservation, setReservation] = useState(null);
   const [user, setUser] = useState(null);
+  const [vehicle, setVehicle] = useState(null);
   const router = useRouter();
   const { id } = router.query;
+  const { currentUser } = useAuth();
 
   useEffect(() => {
     const fetchReservation = async () => {
@@ -26,6 +29,19 @@ const DetailedReservation = () => {
             const userSnap = await getDoc(userRef);
             if (userSnap.exists()) {
               setUser(userSnap.data());
+
+              if (reservationData.vehicleId) {
+                const vehicleRef = doc(
+                  db,
+                  `users/${reservationData.userId}/vehicles/${reservationData.vehicleId}`
+                );
+                const vehicleSnap = await getDoc(vehicleRef);
+                if (vehicleSnap.exists()) {
+                  setVehicle(vehicleSnap.data());
+                } else {
+                  console.log("Vehicle not found");
+                }
+              }
             } else {
               console.log("User not found");
             }
@@ -37,7 +53,7 @@ const DetailedReservation = () => {
     };
 
     fetchReservation();
-  }, [id]);
+  }, [id, currentUser]);
 
   if (!reservation || !user) return <p>Loading...</p>;
 
@@ -64,13 +80,23 @@ const DetailedReservation = () => {
                 Type de réparation : {reservation.reparationType}
               </p>
               <p className="my-2">Détails : {reservation.reparationDetail}</p>
-              <p className="my-2">ID du véhicule : {reservation.vehicleId}</p>
               <p className="my-2">
                 Date de réservation :{" "}
                 {reservation.bookingDate?.toDate().toLocaleString("fr-FR")}
               </p>
               <p className="my-2">Prix : {reservation.price} €</p>
             </div>
+            {vehicle && (
+              <div>
+                <h2 className="font-bold text-lg">Informations du véhicule</h2>
+                <p className="my-2">Année : {vehicle.annee}</p>
+                <p className="my-2">Modèle : {vehicle.modele}</p>
+                <p className="my-2">Marque : {vehicle.marque}</p>
+                <p className="my-2">
+                  Immatriculation : {vehicle.immatriculation}
+                </p>
+              </div>
+            )}
           </div>
         </CardContent>
       </Card>
